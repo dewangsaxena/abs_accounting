f2aea0292e66625eef0065bf7a5321a5ac22d8e4
Dewang Saxena <services@dewang.online>
2024-08-27 12:34:27 -0600

    Testing with PostMark App

--- src/api/modules/utils/email.php
+++ src/api/modules/utils/email.php
@@ -22,7 +22,7 @@ use PHPMailer\PHPMailer\SMTP;
 
 class Email {
     // Service Used
-    private const SERVICE = 'google';
+    private const SERVICE = 'postmarkapp';
     private const FROM_EMAIL = 'abs@abs.company';
 
     // Username
