9f12680dfdb011da12a7f405a75507f022e4f79c
Dewang Saxena <services@dewang.online>
2024-08-28 13:24:20 -0600

    Draft

--- test.php
+++ test.php
@@ -76,10 +76,121 @@ function generate_list(int $store_id) {
     echo $code;
 }
 
+function fetch_inventory(int $store_id): void {
+    $db = get_db_instance();
+    $query = <<<'EOS'
+    SELECT 
+        i.id,
+        i.`identifier`,
+        i.`prices`,
+        inv.`quantity`
+    FROM 
+        items AS i
+    LEFT JOIN 
+        inventory AS inv
+    ON 
+        i.id = inv.item_id
+    WHERE 
+        inv.`store_id` = :store_id;
+    EOS;
+
+    $statement = $db -> prepare($query);
+    $statement -> execute([':store_id' => $store_id]);
+    $result = $statement -> fetchAll(PDO::FETCH_ASSOC);
+    $store_name = StoreDetails::STORE_DETAILS[$store_id]['name'];
+    $code = <<<EOS
+    <html>
+    <head></head>
+    <body>
+    <style>
+    table {
+        border-spacing: 20px 0;
+    }
+    </style>
+    <h1>List for $store_name</h1>
+    <table cellspacing="pixels">
+    <thead>
+    <th align="left">Item Identifier</th>
+    <th align="center">Quantity</th>
+    <th > Price / Item</th>
+    <th align="right">Inventory Value</th>
+    </thead>
+    EOS;
+
+    $total_value = 0;
+    $parts = [];
+    foreach($result as $r) {
+        $id = $r['id'];
+        $identifier = $r['identifier'];
+        $quantity = $r['quantity'];
+        $prices = json_decode($r['prices'], true, flags: JSON_NUMERIC_CHECK| JSON_THROW_ON_ERROR);
+        $price = $prices[$store_id]['buyingCost'] ?? 0;
+        if($quantity < 1 || (($price > 0) === false)) continue;
+        $value = $price * $quantity;
+        $total_value += $value;
+        $value_txt = Utils::number_format($value, 2);
+        $price_txn = Utils::number_format($price, 2);
+
+        if(!isset($parts[$id])) {
+            $parts[$id] = [
+                'identifier' => $identifier,
+                'quantity' => $quantity,
+                'price' => $price,
+            ];
+        }
+    }
+
+    $no_of_items = count($parts);
+    $items = array_keys($parts);
+
+    // Deduct 
+    $amount_to_deduct = 162564.7;
+    while($amount_to_deduct > 50) {
+        $index = rand(0, $no_of_items - 1);
+
+        $selected_item = $items[$index];
+
+        $quantity = $parts[$selected_item]['quantity'];
+        if($quantity > 1) {
+            $price = $parts[$selected_item]['price'];
+            if($price > 1 && ($amount_to_deduct - $price > 0)) {
+                $parts[$selected_item]['quantity'] -= 1;
+                $amount_to_deduct -= $price;
+            }
+        }
+    }
+
+    $total_value = 0;
+
+    foreach($parts as $part) {
+        $identifier = $part['identifier'];
+        $quantity = $part['quantity'];
+        $price_txn = Utils::number_format($part['price'], 2);
+        $value_txt = Utils::number_format($part['price'] * $part['quantity'], 2);
+        $code .= <<<EOS
+        <tr>
+        <td align="left">$identifier</td>
+        <td align="center">$quantity</td>
+        <td ><label style="letter-spacing: 2px;">\$ $price_txn</label></td>
+        <td align="right"><label style="letter-spacing: 2px;">\$ $value_txt</label></td>
+        </tr>
+        EOS;
+
+        $total_value += ($part['price'] * $part['quantity']);
+    }
+
+    $total_value = Utils::number_format($total_value, 2);
+    $code .= "</table><br><br>Total Inventory Value: &nbsp;&nbsp;&nbsp;&nbsp;<label style='letter-spacing: 2px;font-weight:bold;'>\$ $total_value</label>";
+
+    if($amount_to_deduct < 20) echo $code;
+}
+
 if(SYSTEM_INIT_MODE === PARTS) {
-    // generate_list(StoreDetails::CALGARY);
+    // generate_list(StoreDetails::EDMONTON);
+    fetch_inventory(StoreDetails::NISKU);
     // echo 'CALGARY : '. (Correct_IS_BS_Inventory::correct(StoreDetails::CALGARY) ? 'T' : 'F');
 }
+die;
 
 $items = [14942,
 14512, 
@@ -148,7 +259,7 @@ function fetch_quantity_sold_of_items(int $item_id, PDO &$db, int $store_id): in
    return $quantity;
 }
 
-$store_id = StoreDetails::VANCOUVER;
+$store_id = StoreDetails::EDMONTON;
 $db = get_db_instance();
 $quantity_table = [];
 foreach($items as $item_id) {
