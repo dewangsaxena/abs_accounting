d47c56b1fb0883aaf97b550167364a5fc82a6a17
Dewang Saxena <services@dewang.online>
2024-08-29 15:15:48 -0600

    Add last purchase date report in Customer Summary

--- src/api/config/configurations.php
+++ src/api/config/configurations.php
@@ -7,7 +7,7 @@ This file contains configurations used by the application.
 /**
  * Client App Version
  */
-define('CLIENT_APP_VERSION', '2.2.11');
+define('CLIENT_APP_VERSION', '2.2.12');
 
 /* Hosts */
 define('__LOCALHOST__', 0);
--- src/shared/config.ts
+++ src/shared/config.ts
@@ -1,7 +1,7 @@
 /**
  * Configurations used the application.
  */
-export const APP_VERSION = "2.2.11";
+export const APP_VERSION = "2.2.12";
 
 // Stores
 export class Stores {
