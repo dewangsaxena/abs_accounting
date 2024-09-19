33f971f39454a4cdb67ec88512e3ab91e32c2250
Dewang Saxena <services@dewang.online>
2024-09-19 12:46:44 -0600

    Draft

--- test.php
+++ test.php
@@ -188,7 +188,7 @@ if(SYSTEM_INIT_MODE === PARTS) {
     $store_id = StoreDetails::SLAVE_LAKE;
     // generate_list($store_id);
     // fetch_inventory($store_id);
-    die('NISKU : '. (Correct_IS_BS_InventoryV2::correct(StoreDetails::NISKU) ? 'T' : 'F'));
+    die('CALGARY : '. (Correct_IS_BS_InventoryV2::correct(StoreDetails::CALGARY) ? 'T' : 'F'));
 }
 
 $items = [14942,
