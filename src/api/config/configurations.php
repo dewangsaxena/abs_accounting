c479e8e57890cca475f141c585714717e0a82607
Dewang Saxena <services@dewang.online>
2024-12-27 16:11:07 -0700

    Validate New Date when updating new date

--- src/api/config/configurations.php
+++ src/api/config/configurations.php
@@ -8,7 +8,7 @@ This file contains configurations used by the application.
 /**
  * Client App Version
  */
-define('CLIENT_APP_VERSION', '2.2.24');
+define('CLIENT_APP_VERSION', '2.2.25');
 
 /* Hosts */
 define('__LOCALHOST__', 0);
--- src/shared/config.ts
+++ src/shared/config.ts
@@ -1,7 +1,7 @@
 /**
  * Configurations used the application.
  */
-export const APP_VERSION = "2.2.24";
+export const APP_VERSION = "2.2.25";
 
 // Stores
 export class Stores {
