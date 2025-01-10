db9cc20a9c3bbcb16e442c1b10cfd41ed678f790
Dewang Saxena <services@dewang.online>
2025-01-10 11:45:32 -0700

    Changed Items index to identifier instead of code

--- src/api/config/database.sql
+++ src/api/config/database.sql
@@ -203,7 +203,7 @@ CREATE TABLE IF NOT EXISTS items(
     created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     modified TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
-CREATE INDEX idx_items ON items(code);
+CREATE INDEX idx_items ON items(identifier);
 
 /* Inventory */
 CREATE TABLE IF NOT EXISTS inventory(
