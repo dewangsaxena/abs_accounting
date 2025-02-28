e3e8653cbfda592b968fb0044b44dccf9541d4e9
Dewang Saxena <services@dewang.online>
2025-02-28 14:36:14 -0700

    Draft

--- src/api/modules/transactions/shared.php
+++ src/api/modules/transactions/shared.php
@@ -1447,7 +1447,7 @@ class Shared {
             if($transaction_year !== $initial_txn_year) throw new Exception('Cannot change year of transaction.');
 
             // Only I can change the Transaction Date.
-            if($new_date != $old_date) throw new Exception('Cannot Change Date of Transaction.');
+            if(intval($_SESSION['user_id'] ?? 0) !== SpecialExceptions::ROOT && $new_date != $old_date) throw new Exception('Cannot Change Date of Transaction.');
         }
     }
 
