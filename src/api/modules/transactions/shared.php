6dccaf2d3716838fd93ee511d86d0c33e113af47
Dewang Saxena <services@dewang.online>
2025-03-06 14:21:22 -0700

    Draft

--- src/api/modules/transactions/shared.php
+++ src/api/modules/transactions/shared.php
@@ -433,6 +433,12 @@ class Shared {
         // Check whether date,current year transaction is being posted in the same year.
         Shared::validate_new_date_of_transaction($data, $transaction_date);
 
+        // Check for transaction date
+        if(isset($data['initial']['txnDate'])) Shared::check_transaction_older_than_2_days(
+            $data['initial']['txnDate'], 
+            $store_id,
+        );
+
         // Disable Federal Taxes
         $disable_federal_taxes = $data['disableFederalTaxes'] ?? null;
         $disable_provincial_taxes = $data['disableProvincialTaxes'] ?? null;
