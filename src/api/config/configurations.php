2c3a71f286e16d9ea490f2d430e4f4b466a3e523
Dewang Saxena <services@dewang.online>
2025-03-21 13:08:48 -0600

    Added Exception for EHC Items

--- src/api/config/configurations.php
+++ src/api/config/configurations.php
@@ -8,7 +8,7 @@ This file contains configurations used by the application.
 /**
  * Client App Version
  */
-define('CLIENT_APP_VERSION', '2.2.30');
+define('CLIENT_APP_VERSION', '2.2.31');
 
 /* Hosts */
 define('__LOCALHOST__', 0);
--- src/shared/config.ts
+++ src/shared/config.ts
@@ -1,7 +1,7 @@
 /**
  * Configurations used the application.
  */
-export const CLIENT_APP_VERSION = "2.2.30";
+export const CLIENT_APP_VERSION = "2.2.31";
 
 // Stores
 export class Stores {
