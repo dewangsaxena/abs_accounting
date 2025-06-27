61ab75f68080e741b84dd3e71131f751cfc8205f
Dewang Saxena <services@dewang.online>
2025-06-27 15:36:46 -0600

    Updated Client App Version

--- src/api/config/configurations.php
+++ src/api/config/configurations.php
@@ -8,7 +8,7 @@ This file contains configurations used by the application.
 /**
  * Client App Version
  */
-define('CLIENT_APP_VERSION', '2.2.36');
+define('CLIENT_APP_VERSION', '2.2.37');
 
 /* Hosts */
 define('__LOCALHOST__', 0);
