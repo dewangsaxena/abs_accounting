79955933585bffb0b5193a9567933efacc14315f
Dewang Saxena <services@dewang.online>
2025-09-30 14:07:00 -0600

    Draft

--- src/api/config/configurations.php
+++ src/api/config/configurations.php
@@ -8,7 +8,7 @@ This file contains configurations used by the application.
 /**
  * Client App Version
  */
-define('CLIENT_APP_VERSION', '2.2.46');
+define('CLIENT_APP_VERSION', '2.2.44');
 
 /* Hosts */
 define('__LOCALHOST__', 0);
