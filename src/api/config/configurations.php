4e955460afdeb69ca00bcfcd5ae7f0380a8c2b46
Dewang Saxena <services@dewang.online>
2025-12-04 15:33:36 -0700

    Prepended Inventory Tag to escape JSON converting numbered string into numeric types

--- src/api/config/configurations.php
+++ src/api/config/configurations.php
@@ -8,7 +8,7 @@ This file contains configurations used by the application.
 /**
  * Client App Version
  */
-define('CLIENT_APP_VERSION', '2.2.50');
+define('CLIENT_APP_VERSION', '2.2.51');
 
 /* Hosts */
 define('__LOCALHOST__', 0);
