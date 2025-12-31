7c94cd289d65eb466991a2a7b179ded4ca8e1240
Dewang Saxena <services@dewang.online>
2025-12-31 11:56:45 -0700

    Fixed Rounding Bug

--- src/api/modules/transactions/receipt.php
+++ src/api/modules/transactions/receipt.php
@@ -90,8 +90,8 @@ class Receipt {
         }
 
         return [
-            'sum_total' => Utils::round($total_amount_received - $total_discount),
-            'total_discount' => Utils::round($total_discount),
+            'sum_total' => Utils::round($total_amount_received - $total_discount, 4),
+            'total_discount' => Utils::round($total_discount, 4),
             'transactions' => $valid_transactions,
         ];
     }
@@ -169,7 +169,7 @@ class Receipt {
             $type = $txn['type'];
 
             // Amount Received
-            $amount_received = Utils::round($txn['amountReceived'] + $txn['discountGiven']);
+            $amount_received = Utils::round($txn['amountReceived'] + $txn['discountGiven'], 4);
             
             // Discount Given
             $discount_given = $txn['discountGiven'];
@@ -660,7 +660,7 @@ class Receipt {
         $diff = ($date_diff['y'] * 365) + ($date_diff['m'] * 30) + $date_diff['d'];
         $discount_available = 0;
         if($diff < $early_payment_paid_within_days) {
-            $discount_available = Utils::round(($amount_eligible_for_receipt_discount * $early_payment_discount) / 100);
+            $discount_available = Utils::round(($amount_eligible_for_receipt_discount * $early_payment_discount) / 100, 4);
         }
         return $discount_available;
     }
