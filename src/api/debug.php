b7b2772235e06183013091003e07c8b59040a427
Dewang Saxena <services@dewang.online>
2026-02-13 15:03:20 -0700

    Draft

--- src/api/debug.php
+++ src/api/debug.php
@@ -1,17 +1,10 @@
 <?php 
 
 class Debug {
-    private array $data = [];
-
     /**
-     * This method will set attribute.
-     * @param key
-     * @param value
-     * @return void 
+     * <key>: <value>
      */
-    public function set_attribute(string $key, mixed $value): void {
-        $data[$key] = $value;
-    }
+    public static array $data = [];
 
     /**
      * This method will write data to db for debugging.
@@ -19,11 +12,11 @@ class Debug {
      * @param store_id
      * @throws Exception
      */
-    public function write_to_db(PDO &$db, int $store_id): void {
+    public static function write_to_db(PDO &$db, int $store_id): void {
         $statement = $db -> prepare('INSERT INTO debug(store_id, details) VALUES (:store_id, :details);');
         $is_successful = $statement -> execute([
             ':store_id' => $store_id,
-            ':details' => json_encode($this -> data, flags: JSON_NUMERIC_CHECK | JSON_THROW_ON_ERROR),
+            ':details' => json_encode(self::$data, flags: JSON_NUMERIC_CHECK | JSON_THROW_ON_ERROR),
         ]);
         if($is_successful !== true && $statement -> rowCount() < 1) throw new Exception('Unable to Write to Debug Table.');
     }
