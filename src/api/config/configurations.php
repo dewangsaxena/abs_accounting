c0227ee55001cbdc7a3dfefea56a620a89de2756
Dewang Saxena <services@dewang.online>
2026-03-11 11:33:43 -0600

    Bug fix in sales return for status by payment method

--- src/api/config/configurations.php
+++ src/api/config/configurations.php
@@ -8,7 +8,7 @@ This file contains configurations used by the application.
 /**
  * Client App Version
  */
-define('CLIENT_APP_VERSION', '2.3.0');
+define('CLIENT_APP_VERSION', '2.3.1');
 
 /* Hosts */
 define('__LOCALHOST__', 0);
