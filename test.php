31a3ff3dbfd9c60b3ea688ecabefec1436678798
Dewang Saxena <services@dewang.online>
2026-04-13 13:03:46 -0600

    Bug fix

--- test.php
+++ test.php
@@ -12,10 +12,7 @@ require_once "{$_SERVER['DOCUMENT_ROOT']}/src/api/modules/utils/suppressions.php
 require_once "{$_SERVER['DOCUMENT_ROOT']}/src/api/modules/utils/flyer.php";
 require_once "{$_SERVER['DOCUMENT_ROOT']}/src/api/modules/user_management.php";
 require_once "{$_SERVER['DOCUMENT_ROOT']}/src/api/modules/utils/stats.php";
-
-UserManagement::add_root_user();
 die;
-
 function generate_list(int $store_id, bool $do_print=true) {
     $db = get_db_instance();
     $query = <<<'EOS'
