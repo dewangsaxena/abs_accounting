40ab7d22f1dd8f9e9069cdbc46ecc95ddfbe19e4
Dewang <saxenadewang@gmail.com>
2026-04-17 09:52:40 +0530

    Total Reports Profit and cogs margin bug fix

--- src/api/config/configurations.php
+++ src/api/config/configurations.php
@@ -8,7 +8,7 @@ This file contains configurations used by the application.
 /**
  * Client App Version
  */
-define('CLIENT_APP_VERSION', '2.4.0');
+define('CLIENT_APP_VERSION', '2.4.1');
 
 /* Hosts */
 define('__LOCALHOST__', 0);
