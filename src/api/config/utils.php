31b758c8529cae6e677633257778cca413c530b7
Dewang Saxena <services@dewang.online>
2026-07-15 16:22:30 -0600

    Cutoff bug fix

--- src/api/config/utils.php
+++ src/api/config/utils.php
@@ -397,15 +397,16 @@ class Utils {
      * This method will convert UTC timestamp to LocalTime.
      * @param utc_str_timestamp
      * @param store_id
+     * @param use_24_hour_format
      * @return string
      */
-    public static function convert_utc_str_timestamp_to_localtime(string $utc_str_timestamp, int $store_id): string {
+    public static function convert_utc_str_timestamp_to_localtime(string $utc_str_timestamp, int $store_id, bool $use_24_hour_format = false): string {
         date_default_timezone_set('UTC');
         $date_created = date_create($utc_str_timestamp);
         $date_timestamp = date_timestamp_get($date_created);
         $date = date_timestamp_set($date_created, $date_timestamp);
         $date = date_format($date, 'Y-m-d H:i:s');
-        return Utils::get_local_timestamp($date, $store_id);
+        return Utils::get_local_timestamp($date, $store_id, $use_24_hour_format);
     }
 
     /**
